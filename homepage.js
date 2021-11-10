27b289af3c531ec63807aa2c7403938cb266bd9f
Msteve27 <msteve27@vols.utk.edu>
2021-11-10 17:31:14 -0600

    search button works

--- homepage.js
+++ homepage.js
@@ -1,60 +1,33 @@
 let myKey = "0ccf340b422471af8577994f5f5e8925";
-let SearchBtn = document.getElementById("searchBtn");
+let SearchBtnEl = document.getElementById("searchBtn");
 
 
 
-// let getWeather = function(response) {
-//     // format the weather api url
-//     let weatherApiUrl = ("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=0ccf340b422471af8577994f5f5e8925");
-  
-//     // make a get request to url
-//     fetch(weatherApiUrl)
-//       .then(function(response) {
-//         // request was successful
-//         if (response.ok) {
-//           response.json().then(function(weather) {
-//           console.log(weather);
-//           displayWeather(weather);
-//           });
-//         } else {
-//           alert("Error: " + response.statusText);
-//         }
-//       }) 
-//       .catch(function(error) {
-//         alert("Unable to connect to OpenWeather");
-//       });
-//   };
-
-// getWeather();
-// console.log("weather function ran")
-
-// let displayWeather = function(weather) {
-//   console.log("displayWeather ran")
-//   // Loop through the response
-//   for (var i = 0; i < weather.length; i++) {
-//     console.log("Max Temp: " + weather[i].main.temp_max);
-//   }
-
-// };
-
-
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=0ccf340b422471af8577994f5f5e8925")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    // TODO: Loop through the response
-    for (var i = 0; i < data.length; i++) {
-    console.log("Max Temp:");
-    }
-    // TODO: Console log each issue's URL and each user's login
-  });
-
-
+let getWeather = function(response) {
+    
+    // format the weather api url
+    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=0ccf340b422471af8577994f5f5e8925";
+    event.preventDefault();
+    // make a get request to url
+    fetch(weatherApiUrl)
+      .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(weather) {
+          console.log(weather);
+          console.log(weather.main.temp_max);
+          console.log(weather.main.temp_min);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      }) 
+      .catch(function(error) {
+        alert("Unable to connect to OpenWeather");
+      });
+  };
 
 // add event listener to search button 
-// userFormEl.addEventListener("submit", formSubmitHandler);
+SearchBtnEl.addEventListener("click", getWeather);
 
 
