7dd69d38767b90f518f703afec37a78c72f3dc05
Msteve27 <msteve27@vols.utk.edu>
2021-11-21 15:42:07 -0600

    added second fetch

--- fetch.js
+++ fetch.js
@@ -1,6 +1,142 @@
 let myKey = "0ccf340b422471af8577994f5f5e8925";
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${myKey}")
-.then(function (response) {
-  return response.json();
-})
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${myKey}")
+// .then(function (response) {
+//   return response.json();
+// })
+
+
+// let getWeather = function(response) {
+//   // format the weather api url
+//   let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=0ccf340b422471af8577994f5f5e8925";
+
+//   // make a get request to url
+//   fetch(weatherApiUrl)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data)  {
+//       console.log(data);
+//       console.log(data.main.temp_max);
+//       console.log(data.main.temp_min);
+//       // Loop through the response
+//       for (var i = 0; i < data.length; i++) {
+//       console.log("Max Temp: " + data[i].main.temp_max);
+//       }
+//     });  
+      
+// }
+
+// let displayWeather = function(weather) {
+//   console.log("displayWeather ran")
+//   // Loop through the response
+//   for (var i = 0; i < weather.length; i++) {
+//     console.log("Max Temp: " + weather[i].main.temp_max);
+//   }
+
+// };
+
+
+class Fetch {
+    async getCurrent(input) {
+        let response = await fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}'
+        );
+
+        let data = await response.json();
+        return data;
+    }
+}
+
+
+// grab the user input
+const input = document.querySelector(‘input’)
+const searchBtn = document.getElementById(‘searchBtn’)
+const cryptoNameEl = document.getElementById(‘name’)
+const cryptoPriceEl = document.getElementById(‘price’)
+const cryptoRankEl = document.getElementById(‘rank’)
+const cryptoHighEl = document.getElementById(‘high’)
+const asideEl = document.querySelector(‘aside’)
+const searchesDiv = document.getElementById(‘searches’)
+let searches = localStorage.getItem(‘mySearches’)
+						? JSON.parse(localStorage.getItem(‘mySearches’))
+						: localStorage.setItem(‘mySearches’, JSON.stringify([]))
+// if (searches) {
+// 	searches = JSON.parse(localStorage.getItem(‘mySearches’))
+// } else {
+// 	localStorage.setItem(‘mySearches’, JSON.stringify([]))
+// }
+function populateCrypto(cryptos) {
+	let divArray = [...searchesDiv.children]
+	if (searchesDiv.hasChildNodes()) {
+		divArray.forEach(element => {
+			element.remove()
+		});
+	}
+	if (cryptos.length > 0) {
+		cryptos.forEach(crypto => {
+			let aEl = document.createElement(‘a’)
+			aEl.setAttribute(“class”, “block mt-2 bg-gray-300 text-center rounded p-1 search”)
+			aEl.innerText = crypto
+			searchesDiv.appendChild(aEl)
+		})
+	}
+}
+populateCrypto(searches)
+function handleSearch() {
+	// let cryptoId = input.value.toUpperCase()
+	let cryptoId = input.value.toUpperCase()
+	let url = `https://api.nomics.com/v1/currencies/ticker?key=1c9290f94cc5bd355453afe72dc1fa4d89225b52&ids=${cryptoId}&interval=1d,7d,30d,365d,ytd&convert=USD&per-page=100&page=1`
+	
+	if (cryptoId) {
+		// make a fetch call => function
+		getCryptoData(url)
+	}
+	
+	input.value = “”
+}
+function getCryptoData(url) {
+	fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		console.log(‘data: ’, data);
+		let crypto = data[0]
+		populateData(crypto)
+		let searches = JSON.parse(localStorage.getItem(‘mySearches’))
+		searches.push(crypto.id)
+		let uniqueSearches =Array.from(new Set(searches))
+		console.log(‘uniqueSearches: ’, uniqueSearches);
+		localStorage.setItem(‘mySearches’, JSON.stringify(uniqueSearches))
+		// let aEl = document.createElement(‘a’)
+		// aEl.setAttribute(“class”, “block mt-2 bg-gray-300 text-center rounded p-1”)
+		// aEl.innerText = crypto.id
+		// asideEl.appendChild(aEl)
+		populateCrypto(uniqueSearches)
+	})
+	.catch(err => {
+		if (err) alert(‘bad input’)
+	})
+}
+function populateData(crypto) {
+	// populate the main section with the data
+	cryptoNameEl.textContent = `Name: ${crypto.name}`
+	cryptoPriceEl.textContent = `Price: $${crypto.price}`
+	cryptoRankEl.textContent = `Rank: ${crypto.rank}`
+	cryptoHighEl.textContent = `High: ${crypto.high}`
+	
+	let cryptoLogoEl = document.getElementById(‘logo’)
+	cryptoLogoEl.setAttribute(‘src’, crypto.logo_url)
+	cryptoLogoEl.setAttribute(‘alt’, crypto.name + ‘logo’)
+	cryptoLogoEl.removeAttribute(‘hidden’)
+}
+function refetchCrypto(event) {
+	if (event.target.classList.value.includes(“search”)) {
+		let crypto = event.target.innerText
+	
+		let url = `https://api.nomics.com/v1/currencies/ticker?key=1c9290f94cc5bd355453afe72dc1fa4d89225b52&ids=${crypto}&interval=1d,7d,30d,365d,ytd&convert=USD&per-page=100&page=1`
+	
+		getCryptoData(url)
+	}
+}
+asideEl.addEventListener(‘click’, refetchCrypto)
+searchBtn.addEventListener(‘click’, handleSearch)
--- homepage.js
+++ homepage.js
@@ -1,46 +1,88 @@
 let myKey = "0ccf340b422471af8577994f5f5e8925";
 let SearchBtnEl = document.getElementById("searchBtn");
 let currentWeatherEl = document.getElementById("current-weather");
+let searchCityEl = document.getElementById("city");
+let cityDisplayEl = document.getElementById("cityDisplay");
 
 
 
-let getWeather = function(response) {
-    
+let getWeather = function(city) {
     // format the weather api url
-    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=0ccf340b422471af8577994f5f5e8925";
+    let searchCity = searchCityEl.value
+    // searches city and grabs lat and lon
+    let weatherLatLonUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${myKey}`; 
     event.preventDefault();
-    // make a get request to url
-    fetch(weatherApiUrl)
+
+    // make a get request to url to grab lat and lon
+    fetch(weatherLatLonUrl)
       .then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(weather) {
-          console.log(weather);
-          console.log(weather.main.temp_max);
-          console.log(weather.main.temp_min);
+          response.json().then(function(weather) { 
+          console.log(weather)
 
           let currentCityItem = document.createElement('h1');
-          currentCityItem.textContent = weather.name;
+          currentCityItem.textContent = "City: " + weather.name;
           currentWeatherEl.appendChild(currentCityItem);
 
           let currentTempItem = document.createElement('li');
-          currentTempItem.textContent = weather.main.temp + " °F";
+          currentTempItem.textContent = "Current Temp: " + weather.main.temp + " °F";
           currentTempItem.style.listStyle = "none";
           currentWeatherEl.appendChild(currentTempItem);
-
+        
           let currentWindItem = document.createElement('li');
-          currentWindItem.textContent = weather.wind.speed + " MPH";
+          currentWindItem.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
           currentWindItem.style.listStyle = "none";
           currentWeatherEl.appendChild(currentWindItem);
-
+        
           let currentHumidItem = document.createElement('li');
-          currentHumidItem.textContent = weather.main.humidity + " %";
+          currentHumidItem.textContent = "Humidity: " + weather.main.humidity + " %";
           currentHumidItem.style.listStyle = "none";
           currentWeatherEl.appendChild(currentHumidItem);
+        
+          let lonEL = weather.coord.lon
+          let latEl = weather.coord.lat 
+          let weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latEl}&lon=${lonEL}&appid=${myKey}`;
+          // make a get request to onecall api  
+        fetch(weatherApiUrl)
+          .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(weather) {
+          console.log(weather)
+          console.log(weather.current.temp)
+          
+          let today = new Date().toISOString().slice(0, 10)
+          console.log(today)
 
+          let currentDayItem = document.createElement('h3');
+          currentDayItem.textContent = "Date: " + today;
+          currentWeatherEl.appendChild(currentDayItem);
 
+          let currentUvItem = document.createElement('h3');
+          currentUvItem.textContent = "UV: " + weather.current.uvi;
+          if (weather.current.uvi <= 2) {
+            currentUvItem.style.backgroundColor = "green";
+          } 
+          if (weather.current.uvi > 2 && weather.current.uvi < 7) {
+            currentUvItem.style.backgroundColor = "orange";
+          }
+          if (weather.current.uvi > 7 && weather.current.uvi <= 12){
+            currentUvItem.style.backgroundColor = "red";
+          }
+          currentWeatherEl.appendChild(currentUvItem);
 
-
+          
+          
+      
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      }) 
+      .catch(function(error) {
+        alert("Unable to connect to OpenWeather");
+      });  
           });
         } else {
           alert("Error: " + response.statusText);
@@ -49,11 +91,27 @@ let getWeather = function(response) {
       .catch(function(error) {
         alert("Unable to connect to OpenWeather");
       });
-  };
+  
+};
+
+
+  
+
+
+
+
+
 
 // add event listener to search button 
 SearchBtnEl.addEventListener("click", getWeather);
+          
+              
 
-// let displayWeather = function(weather) {
-  
-// }
+
+
+
+// 
+
+          
+
+          
